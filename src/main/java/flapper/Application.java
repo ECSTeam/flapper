f091832daabea1dd69722729c1b6b8a6cb7fcebe
Josh Ghiloni <jghiloni@ecsteam.com>
2016-08-29 09:20:20 -0600

    Randomize Flap interval

--- src/main/java/flapper/Application.java
+++ src/main/java/flapper/Application.java
@@ -1,5 +1,7 @@
 package flapper;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,24 +10,28 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("starting the app...");
-		
+
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
-		
+
+		Random r = new Random();
+
+		int offset = r.nextInt(15) - 7;
+
 		try {
-			Thread.sleep(15000);
+			Thread.sleep(10000 + (1000 * offset));
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 
 		//force a quit
 		System.out.println("forcing a quit...");
 		System.exit(1);
-		
-		
+
+
 	}
 
 }
